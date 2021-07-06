c63d4162ebdd00c19e00bbf45fc5bde62dceccb0
Daniel <cdanielask@gmail.com>
2021-07-06 19:43:17 +1000

    Adds complex state and start to update styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,18 @@
 import React, {useContext} from 'react'
 import { AuthContext } from '../contexts/AuthProvider';
+import { Container } from '@material-ui/core';
 
 export default function Home() {
 	const [auth, setAuth] = useContext(AuthContext);
 
 	return (
-		<div>
+		<Container maxWidth='sm'>
 				{auth.loggedIn ? (
 					<div>
 					<h1>LOGGED IN</h1>
 					<h2>Welcome {auth.username}</h2>
 					</div>
 					) : <h1> NOT LOGGED IN</h1>}
-		</div>
+		</Container>
 	)
 }
--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,16 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from "react";
 
-export const AuthContext = React.createContext()
+export const AuthContext = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+		case "login":
+			localStorage.setItem('token',action.token)
+			return {
+				...state,
+				loggedIn: true,
+				username: action.username,
+				email: action.email
+			};
+    case "sign-out":
+			localStorage.removeItem('token');
+      return {
+				loggedIn: false
+			};
+    default:
+      return state;
+  }
+};
 
 export default function AuthProvider({ children }) {
-	const [auth, setAuth] = useState({
-		loggedIn : !!localStorage.getItem('token'),
-		username: localStorage.getItem('username') 
-	})
+  const [state, dispatch] = useReducer(reducer, {
+    loggedIn: !!localStorage.getItem("token"),
+    username: localStorage.getItem("username"),
+  });
+  const [auth, setAuth] = useState({
+    loggedIn: !!localStorage.getItem("token"),
+    username: localStorage.getItem("username"),
+  });
 
-	return (
-		<AuthContext.Provider value={[auth,setAuth]}>
-			{children}
-		</AuthContext.Provider>
-	)
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
