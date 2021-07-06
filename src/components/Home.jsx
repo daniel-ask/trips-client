30222e9d7d217861c81a73a8d788033b3b24f5d7
Daniel Ask <cdanielask@gmail.com>
2021-07-07 00:51:16 +1000

    Updated styles to use material ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,11 +8,11 @@ export default function Home() {
 	return (
 		<Container maxWidth='sm'>
 				{auth.loggedIn ? (
-					<div>
+					<div style={{textAlign: 'center'}}>
 					<h1>LOGGED IN</h1>
 					<h2>Welcome {auth.username}</h2>
 					</div>
-					) : <h1> NOT LOGGED IN</h1>}
+					) : <h1 style={{textAlign: 'center'}}> NOT LOGGED IN</h1>}
 		</Container>
 	)
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,18 @@
 import React, { useState, useContext } from 'react';
 import {AuthContext} from '../contexts/AuthProvider';
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {Link} from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import EnhancedEncryptionOutlinedIcon from '@material-ui/icons/EnhancedEncryptionOutlined';
 
-export default function SignUp({history}) {
+export function Login({history}) {
 	
 	const [auth,setAuth] = useContext(AuthContext);
 	const [signUpForm, setSignUpForm] = useState({
@@ -72,4 +83,184 @@ export default function SignUp({history}) {
 		</form>
 		</div>
 	)
+}
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp({ history }) {
+  const classes = useStyles();
+
+	const [auth,setAuth] = useContext(AuthContext);
+	const [signUpForm, setSignUpForm] = useState({
+		user:{
+			username: '',
+			email: '',
+			password: '',
+			password_confirmation: ''
+		}
+	})
+
+	const [errorMessage, setErrorMessage] = useState('')
+
+	const changeInput = e => {
+		setSignUpForm({
+			user:{
+				...signUpForm.user,
+				[e.target.name] : e.target.value
+			}
+		})
+	}
+
+	const postSignUp = async () => {
+		const response = await fetch('http://localhost:3000/sign-up',{
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(signUpForm)
+		})
+
+		const data = await response.json();
+		if(data.token){
+			localStorage.setItem('token', data.token)
+			localStorage.setItem('username', data.username)
+      setAuth({
+        ...auth,
+        username: data.username,
+        email: data.email,
+        loggedIn: true,
+      });
+			history.push('/')
+		}else{
+			setErrorMessage(data)
+		}
+		console.log(data)
+	}
+
+	const signUpUser = e => {
+		e.preventDefault()
+		postSignUp()
+	}
+	
+	const {username, email, password, password_confirmation} = signUpForm.user
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <EnhancedEncryptionOutlinedIcon/>
+        </Avatar>
+        <Typography component="h1" variant="h5">
+					Sign Up
+        </Typography>
+        <Typography component="h1" variant="h6" style={{color: 'darkred'}}>
+          {errorMessage}
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={signUpUser}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoFocus
+            value={username}
+            onChange={changeInput}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="email"
+            label="Email"
+            type="email"
+            id="email"
+            autoComplete="current-password"
+            value={email}
+            onChange={changeInput}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={changeInput}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password_confirmation"
+            label="Confirm Password"
+            type="password"
+            id="password_confirmation"
+            autoComplete="current-password"
+            value={password_confirmation}
+            onChange={changeInput}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link to="/login" variant="body2">
+                {"Already have an account? Login"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
